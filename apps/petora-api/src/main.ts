7129d17637faaacadac72bd824c400d7e17f93e5
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-05-26 08:05:01 +0900

    fix: integrate global pipe validation

--- apps/petora-api/src/main.ts
+++ apps/petora-api/src/main.ts
@@ -1,8 +1,10 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
+	app.useGlobalPipes(new ValidationPipe());
 	await app.listen(process.env.PORT ?? process.env.PORT_API ?? 4000);
 	console.log(`Petora-API servier is running in PORT: "http://localhost:${process.env.PORT_API}"`);
 }
