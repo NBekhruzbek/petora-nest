98471d0a71f052d4d933c92cf04cbbe12f178f66
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-05-27 21:14:21 +0900

    feat: creation and verification jwt

--- apps/petora-api/src/components/member/member.service.ts
+++ apps/petora-api/src/components/member/member.service.ts
@@ -19,7 +19,7 @@ export class MemberService {
 
 		try {
 			const result = await this.memberModel.create(input);
-			// TODO: Authentication via TOKEN
+			result.accessToken = await this.authService.createToken(result);
 			return result;
 		} catch (err) {
 			console.log('ERROR, Service.model: ', err instanceof Error ? err.message : err);
@@ -40,9 +40,10 @@ export class MemberService {
 			throw new InternalServerErrorException(Message.BLOCKED_USER);
 		}
 
-		// TODO: Compare Passwords
 		const isMatch = await this.authService.comparePasswords(memberPassword, response.memberPassword);
 		if (!isMatch) throw new InternalServerErrorException(Message.WRONG_PASSWORD);
+
+		response.accessToken = await this.authService.createToken(response);
 
 		return response;
 	}
