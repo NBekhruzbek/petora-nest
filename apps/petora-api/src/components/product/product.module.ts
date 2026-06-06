02da52291f395f8cdc0055199947901ca01aee6d
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-06-06 17:20:50 +0900

    fix: modification of Product Module

--- apps/petora-api/src/components/product/product.module.ts
+++ apps/petora-api/src/components/product/product.module.ts
@@ -1,4 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ProductService } from './product.service';
+import { ProductResolver } from './product.resolver';
+import { MongooseModule } from '@nestjs/mongoose';
+import ProductSchema from '../../schemas/Product.model';
+import { AuthModule } from '../auth/auth.module';
+import { ViewModule } from '../view/view.module';
 
-@Module({})
+@Module({
+	imports: [
+		MongooseModule.forFeature([
+			{
+				name: 'Product',
+				schema: ProductSchema,
+			},
+		]),
+		AuthModule,
+		ViewModule,
+	],
+	providers: [ProductService, ProductResolver],
+})
 export class ProductModule {}
