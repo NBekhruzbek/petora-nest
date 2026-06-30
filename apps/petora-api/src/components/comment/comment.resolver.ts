a83aca5f3c8e4b7f16119414c0d4d978ae22bc96
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-06-30 22:42:49 +0900

    feat: develop UpdateComment GraphQL API

--- apps/petora-api/src/components/comment/comment.resolver.ts
+++ apps/petora-api/src/components/comment/comment.resolver.ts
@@ -6,6 +6,8 @@ import { Comment } from '../../libs/dto/comment/comment';
 import { CommentInput } from '../../libs/dto/comment/comment.input';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { Types } from 'mongoose';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class CommentResolver {
@@ -19,5 +21,16 @@ export class CommentResolver {
 	): Promise<Comment> {
 		console.log('Mutation: createComment');
 		return await this.commentService.createComment(memberId, input);
+	}
+
+	@UseGuards(AuthGuard)
+	@Mutation((returns) => Comment)
+	public async updateComment(
+		@Args('input') input: CommentUpdate,
+		@AuthMember('_id') memberId: Types.ObjectId,
+	): Promise<Comment> {
+		console.log('Mutation: updateComment');
+		input._id = shapeIntoMongoObjectId(input._id);
+		return await this.commentService.updateComment(memberId, input);
 	}
 }
--- apps/petora-api/src/components/comment/comment.service.ts
+++ apps/petora-api/src/components/comment/comment.service.ts
@@ -1,12 +1,13 @@
 import { BadRequestException, Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model, Types } from 'mongoose';
 import { Comment } from '../../libs/dto/comment/comment';
 import { QnaService } from '../qna/qna.service';
 import { BoardArticleService } from '../board-article/board-article.service';
 import { CommentInput } from '../../libs/dto/comment/comment.input';
+import { CommentGroup, CommentStatus } from '../../libs/enums/comment.enum';
 import { Message } from '../../libs/enums/common.enum';
-import { CommentGroup } from '../../libs/enums/comment.enum';
+import { Model, Types } from 'mongoose';
+import { CommentUpdate } from '../../libs/dto/comment/comment.update';
 
 @Injectable()
 export class CommentService {
@@ -46,6 +47,23 @@ export class CommentService {
 		}
 
 		if (!result) throw new InternalServerErrorException(Message.CREATE_FAILED);
+		return result;
+	}
+
+	public async updateComment(memberId: Types.ObjectId, input: CommentUpdate): Promise<Comment> {
+		const { _id } = input;
+		const result = await this.commentModel.findOneAndUpdate(
+			{
+				_id: _id,
+				memberId: memberId,
+				commentStatus: CommentStatus.ACTIVE,
+			},
+			input,
+			{
+				new: true,
+			},
+		);
+		if (!result) throw new InternalServerErrorException(Message.UPDATE_FAILED);
 		return result;
 	}
 }
