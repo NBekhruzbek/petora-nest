65a04d666e6bd532ee2334242ddea461a596c0b1
NBekhruzbek <bekhruzbek2022@gmail.com>
2026-07-06 22:06:12 +0900

    feat: connect to MongoDB & modify batch.module

--- apps/petora-batch/src/petora-batch.module.ts
+++ apps/petora-batch/src/petora-batch.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { PetoraBatchController } from './petora-batch.controller';
 import { PetoraBatchService } from './petora-batch.service';
 import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
+import { ScheduleModule } from '@nestjs/schedule';
 
 @Module({
-	imports: [ConfigModule.forRoot()],
+	imports: [ConfigModule.forRoot(), DatabaseModule, ScheduleModule.forRoot()],
 	controllers: [PetoraBatchController],
 	providers: [PetoraBatchService],
 })
